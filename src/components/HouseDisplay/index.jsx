99ea1cb40091e1f82061e799c2dee63991c27585
Lucas Zemlin <lzemlin@LABA.PSWAS.LOCAL>
2023-09-15 12:56:22 -0700

    added key

--- src/components/HouseDisplay/index.jsx
+++ src/components/HouseDisplay/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const HouseDisplay = ({ houses }) => {
     return (
-        <div>{houses.map((house) => {
-            return (<>
+        <div>{houses.map((house, index) => {
+            return (
+            <div key={house.address + index}>
                 <h2>{house.name}</h2>
                 <p>{house.address}</p>
                 <p>${house.price}</p>
                 <p>{house.isSold.toString()}</p>
-            </>)
+            </div>)
         })}</div>
     )
 }
